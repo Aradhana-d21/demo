385bdab0659aa6261426f9d2fb185c2cefa95156
Aradhana-d21 <106795765+Aradhana-d21@users.noreply.github.com>
2022-06-09 18:07:48 +0530

    Add files via upload

---
+++ animals.java
@@ -0,0 +1,68 @@
+package oops;
+class Animal {
+
+    public void eat()
+    {
+        System.out.println("eat method");
+
+    }
+    public void sleep()
+    {
+        System.out.println("sleep method");
+
+    }
+
+}
+class Bird extends Animal{
+    
+    public void eat() {
+        super.eat();
+        System.out.println("overide eat");
+    }
+
+    
+    public void sleep() {
+        super.sleep();
+        System.out.println("override sleep");
+    }
+
+    public void fly()
+    {
+        System.out.println("in fly method");
+
+    }
+}
+class WildAnimal extends Animal{
+    
+    public void eat() {
+        super.eat();
+        System.out.println("overide eat");
+    }
+    public void sleep() {
+        super.sleep();
+        System.out.println("override sleep");
+    }
+
+    public void fly()
+    {
+        System.out.println("in fly method");
+
+    }
+}
+
+class Animals{
+    public static void main(String[] args) {
+        Animal a =new Animal();
+        Bird b = new Bird();
+        WildAnimal w=new WildAnimal();
+        w.eat();
+        w.sleep();
+        a.eat();
+        a.sleep();
+        b.eat();
+        b.sleep();
+        b.fly();
+    }
+}
+
+
